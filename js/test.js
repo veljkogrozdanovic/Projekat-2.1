8318a2746054a25ff89389ff8f5eb67696f08617
veljko grozdanovic <veljkoas@yahoo.com>
2018-07-29 10:52:31 +0200

    ...

--- js/test.js
+++ js/test.js
@@ -1,3 +1,6 @@
+window.onload = loadF;
+
+  
 let num = 1;
 function inputF() {
    let  inputTask = document.getElementById("inputText").value;
@@ -18,8 +21,12 @@ function inputF() {
     document.getElementById("box").appendChild(newDiv);
     document.getElementById("inputText").value = "";
     document.getElementById("inputText").placeholder = "New task";
+    document.getElementById("filter").value = "";
+    document.getElementById("filter").placeholder = "Filter Task"
+    filterF();
     saveF();
     num++;
+    document.getElementById("inputText").focus();
    }
 }
 
@@ -32,7 +39,10 @@ let test = confirm("Da li zelis da obrises ovaj task?");
         let newTask= document.getElementById(x);
         //console.log(newTask);
         newTask.parentNode.remove(newTask);
+        document.getElementById("filter").value = "";
+        document.getElementById("filter").placeholder = "Filter Task"
         saveF();
+        filterF()
     }
 }
 
@@ -40,12 +50,14 @@ function deleteAllF() {
     let test = confirm("Da li zelis da obrises sve taskove?")
     if (test === true) {
 document.getElementById("box").innerHTML = "";
+document.getElementById("filter").value = "";
+document.getElementById("filter").placeholder = "Filter Task"
 saveF();
     }
 }
 
 function filterF() {
-   let filterText = document.getElementById("filter").value;
+   let filterText = document.getElementById("filter").value.toLowerCase();
    //console.log(filterText);
   let allP =  document.getElementById("box").getElementsByTagName("p");
   for (let i = 0; i < allP.length; i++) {
@@ -53,7 +65,7 @@ function filterF() {
       //console.log(pSmall);
       if(pSmall.includes(filterText) === true) {
         allP[i].parentNode.style.display = "block";
-         console.log("da");
+         //console.log("da");
       }
       else {
          allP[i].parentNode.style.display = "none";
@@ -61,16 +73,16 @@ function filterF() {
       }
   }
 }
-
 function saveF() {
     localStorage.setItem("memo", document.getElementById("box").innerHTML);
 }
 
-window.onload = loadF;
+
 function loadF() {
  document.getElementById("box").innerHTML = localStorage.getItem("memo");
  let x = document.getElementById("box").getElementsByTagName("div");
  for (let i = 0; i < x.length; i ++) {
     x[i].style.display = "block"
+    console.log(document.getElementById("filter").value);
  }
 }
